ff7bf2e06f0328dbf68fc2aae59074cf70288c28
Rodrigo Franco <ralmeidafranco@gmail.com>
2019-12-02 17:28:43 -0300

    ...

--- src/main/java/br/usjt/temprevisao/controller/TemposController.java
+++ src/main/java/br/usjt/temprevisao/controller/TemposController.java
@@ -3,12 +3,22 @@ package br.usjt.temprevisao.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
+
+import br.usjt.temprevisao.core.Forecast;
+import  br.usjt.temprevisao.model.Cidade;
+import  br.usjt.temprevisao.model.Clima;
+
+import br.usjt.temprevisao.repository.*;
 import br.usjt.temprevisao.model.Tempo;
+import br.usjt.temprevisao.service.PrevisaoService;
 import br.usjt.temprevisao.service.TemposService;
 
 import java.util.List;
@@ -17,22 +27,14 @@ import java.util.List;
 @Controller
 public class TemposController {
 
-//    @Autowired
-//    private PeriodosRepository repository;
-
-//    @Autowired
-//    public PeriodosController(PeriodosRepository repository) {
-//        this.repository = repository;
-//    }
-//
-//    @Autowired
-//    public void setRepository(PeriodosRepository repository){
-//        this.repository = repository;
-//    }
 
     @Autowired
     private TemposService temposService;
+    @Autowired
+    private CidadeRepository cidadeRepo;
     
+    @Autowired
+    private PrevisaoService previsaoService;
     /*------------------------------------------------Model and View-----------------------------------------------------------------*/
 
     @GetMapping("/tempo")
@@ -64,17 +66,23 @@ public class TemposController {
         mv.addObject("tempos", tempos);
         return mv;
     }
-    
-    //Salvar Tempo
 
     @PostMapping("/tempo")
     public ModelAndView Home() {
         ModelAndView mv = new ModelAndView("lista_tempo");
         mv.addObject(new Tempo());
-        //List<Tempo> tempos = repository.findAll();
         List<Tempo> tempos = temposService.listarTodos();
         mv.addObject("tempos", tempos);
         return mv;
+    }
+    
+    @GetMapping(value = "/buscarPrev/cidade/{cidadeId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String consume(@PathVariable long cidadeId) {
+        Cidade cidade = cidadeRepo.findById(cidadeId).get(); 
+        Forecast forecast = previsaoService.obter(cidade);
+        List<Clima> climas = previsaoService.toClimaList(forecast, cidade);
+        previsaoService.saveClimas(climas);
+        return "redirect:/tempo";
     }
    
 }
--- src/main/java/br/usjt/temprevisao/repository/CidadeRepository.java
+++ src/main/java/br/usjt/temprevisao/repository/CidadeRepository.java
@@ -4,10 +4,12 @@ package br.usjt.temprevisao.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.scheduling.annotation.Async;
 
 import br.usjt.temprevisao.model.Cidade;
 
 import java.util.List;
+import java.util.concurrent.Future;
 
 public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 
@@ -17,5 +19,11 @@ public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 
     @Query("select c from Cidade c where c.latitude = :latitude and c.longitude = :longitude")
     public List<Cidade> findAllByLatitudeAndLongitude(@Param("latitude") Double latitude, @Param("longitude") Double longitude);
+    
+    public Cidade findOneByLatitudeAndLongitude(Double latitude, Double longitude);
+    public List<Cidade> findByNomeContainingIgnoreCase(String nome);
+    
+    @Async
+    public Future<List<Cidade>> findByNome(String nome);
 
 }
